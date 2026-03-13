7058fb6622abefe642616ba7c0353c4bbe5da673
José Alves <71458201+mercadao-portel@users.noreply.github.com>
2026-03-13 13:23:49 -0300

    Add files via upload
    Primeira versão PWA CP

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+
+const CACHE_NAME='pwa-cp-final';
+const urlsToCache=['./','./index.html','./style.css','./app.js','./manifest.json'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)))});
+self.addEventListener('fetch',event=>{event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request)))});
